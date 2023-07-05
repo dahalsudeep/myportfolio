553f2d64c1c802dc89339e5a817e28263f6b246f
dahalsudeep <dahalsudeep890@gmail.com>
2023-07-05 13:16:26 +0100

    device responsive half edited

--- Java Script/jscript.js
+++ Java Script/jscript.js
@@ -1,3 +1,13 @@
+// toggle navigation--------------
+let menuBtn = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menuBtn.onclick = () => {
+    menuBtn.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+
 
 // Type Animation---------------
 
@@ -24,7 +34,28 @@ ScrollReveal().reveal('.home-photo, .services-content, .portfolio-box, .contact 
 ScrollReveal().reveal('.home-content h1, .about-img', {origin:'left'});
 ScrollReveal().reveal('.home-content p, .about-content', {origin:'right'});
 
-function myFunction() {
-    document.getElementById("menu-btn").toggleAttribute = (".navbar");
-}
+// active link-------------------------
 
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset +height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+
+// remove menubtn on navbar click-----------
+menuBtn.classList.remove('bx-x');
+navbar.classList.remove('active');
